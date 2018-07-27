d6afcc3434c1281a09a9042eb5cb11ad9a6bded6
eugen.zha <nightskylark@users.noreply.github.com>
2018-07-27 18:06:28 +0300

    Disallow getBoundingClientRect on a server

--- testing/helpers/ssrEmulator.js
+++ testing/helpers/ssrEmulator.js
@@ -33,7 +33,7 @@ var serverSideDOMAdapter = require("./serverSideDOMAdapterPatch.js");
     document.createElement = function() {
         var result = originalCreateElement.apply(this, arguments);
 
-        ["offsetWidth", "offsetHeight"].forEach(function(field) {
+        ["offsetWidth", "offsetHeight", "getBoundingClientRect"].forEach(function(field) {
             Object.defineProperty(result, field, {
                 get: function() {
                     return undefined;
